cab459ce1092d6f624abd1c002924cce7209f4ee
Karl Kirch <kkirch@me.com>
2012-02-07 20:22:29 -0600

    fixed timestep game loop
    Interpolated rendering
    Added sdl event handling to IGame protocol

--- KobjcEngine/IGame.h
+++ KobjcEngine/IGame.h
@@ -5,6 +5,7 @@
 @protocol IGame <NSObject>
 
 @optional
+//engine hooks
 -(void)initGame;
 -(void)preUpdate;
 -(void)postUpdate;
@@ -12,7 +13,11 @@
 -(void)postRender;
 -(void)shutdown;
 
+//sdl event handling
+-(void)handleSDLEvent:(const SDL_Event *)event;
+
 @required
+//basics of the game loop
 -(void)update:(long)dt;
 -(void)render:(float)i withSurface:(SDL_Surface *)surface;
 
--- KobjcEngine/Kobjc.m
+++ KobjcEngine/Kobjc.m
@@ -24,37 +24,63 @@
 	//show window
 	SDL_Surface* surface = SDL_SetVideoMode( 640, 480, 16, SDL_OPENGL ); 
 
+	//init hook
 	if ([game respondsToSelector:@selector(initGame)]) {
 		[game initGame];
 	}
 
 	SDL_Event event;
 	BOOL running = true;
+	//cache whether or not the game responds to the following engine hooks
 	BOOL respondsToPreUpdate = [game respondsToSelector:@selector(preUpdate)];
 	BOOL respondsToPostUpdate = [game respondsToSelector:@selector(postUpdate)];
 	BOOL respondsToPreRender = [game respondsToSelector:@selector(preRender)];
 	BOOL respondsToPostRender = [game respondsToSelector:@selector(postRender)];
+	BOOL respondsToSDLEventUpdate = [game respondsToSelector:@selector(handleSDLEvent:)];
+
+	const int UPDATES_PER_SECOND = 30;
+	const int MS_PER_UPDATE = 1000 / UPDATES_PER_SECOND;  //fixed update rate
+	const int MAX_FRAMESKIP = 10; //maximum skips frame skips per update
+	Uint32 nextUpdate = SDL_GetTicks();
+	int skipLoops;
+	float interpolation;
 
 	while(running) {
+		skipLoops = 0;
+
 		while(SDL_PollEvent(&event)) {
 			switch(event.type) {
 				case SDL_QUIT:
 				running = false;
 				break;
 			}
+			if (respondsToSDLEventUpdate) {
+				[game handleSDLEvent:&event];
+			}
 		}
 
-		if (respondsToPreUpdate) [game preUpdate];
-		[game update:-1];
-		if (respondsToPostUpdate) [game postUpdate];
 
+		while(SDL_GetTicks() > nextUpdate && skipLoops < MAX_FRAMESKIP)
+		{
+			//update hooks
+			if (respondsToPreUpdate) [game preUpdate];
+			[game update:MS_PER_UPDATE];
+			if (respondsToPostUpdate) [game postUpdate];
+			nextUpdate += MS_PER_UPDATE;
+			skipLoops++;
+		}
+
+		interpolation = (float)(SDL_GetTicks() + MS_PER_UPDATE - nextUpdate) / (float)( MS_PER_UPDATE );
+
+		//render hooks
 		if (respondsToPreRender) [game preRender];
-		[game render:-1 withSurface:surface];
+		[game render:interpolation withSurface:surface];
 		if (respondsToPostRender) [game postRender];
 
 		SDL_Delay(1);
 	}
 
+	//shutdown hook
 	if ([game respondsToSelector:@selector(shutdown)]) {
 		[game shutdown];
 	}
